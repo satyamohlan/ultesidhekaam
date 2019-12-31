4295a0cb1ec46d3d7ce02495cef6b175c24feded
satyam ohlan <satyam.ohlan1998@gmail.com>
2019-12-31 20:46:07 +0530

    harsh ka kaaam

--- ultesidhekaam.ino
+++ ultesidhekaam.ino
@@ -10,33 +10,13 @@ const int trigPin_s = 4;                  //straight sensor trigger pin
 const int echoPin_s = 8;                 //left sensor echo pin
 const int trigPin_b = 26;           
 const int echoPin_b = 2;
- //establish variables for duration of the ping, and the distance result in cm
-int lm_status, rm_status;         //left motor status, right motor status, 0 = none , 1 = forward, -1 = backward
-int tiles = 0;
-int pref[3] = {1, -2, 2};
-int tiles_to_cover = 32;
-int pos[2] = {1, 1};
-int len[2] = {4, 8};
-int arr[200];
-int last[200];
-int count = 0;
-int togo[200] = {};
-int tgcount;
 int rotation = 0;
 bool right = false;
 bool left = false;
 bool front = false;
-String direction = "forward";
-int lstcounter = 0;
-int corner = 0;
 int checkpoint=0;
 int hazarder = 0;
 int victimer=0;
-/*const int trigPin= 11;
-  const int echoPin= 12;
-  const int trigPin2= 7;
-  const int echoPin2= 6;
-*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup() {
   Serial.begin(9600);
   pinMode(lmf, OUTPUT);
@@ -51,11 +31,6 @@ void setup() {
   pinMode(echoPin_r, INPUT);
   pinMode(13, INPUT);
   pinMode(20, INPUT);
-  for (int x = 0; x < 200; x++) {
-    arr[x] = 0;
-    last[x] = 0;
-    togo[x] = 0;
-  }
 visitall();
 }
 
@@ -73,26 +48,12 @@ long microsecondsToCentimeters(long microseconds) {
 }
 
 void leftTurn() {
-  bool x;
-  bool prevstate;
-  rotation = 0;
 
-  int stripe = 0;
-  x = digitalRead(13);
-  if (x == HIGH) {
-    stripe = 8;
-  }
-  if (x == LOW) {
-    stripe = 8;
-  }
+  int stripe = 8;
   while ((true)) {
     // put your main code here, to run repeatedly:
 
-    prevstate = x;
-    x = digitalRead(13);
-    if (x != prevstate && x == 1) {
-      rotation++;
-    }
+     ir();
     if (rotation <= stripe) {
       analogWrite(rmf, 155);
       analogWrite(lmb, 155);
@@ -107,72 +68,15 @@ void leftTurn() {
 
 
   }
-
-
-}
-void uturn () {
-
-  bool x;
-  bool prevstate;
-  float distance = 0;
-  int stripe = 0;
-  rotation = 0;
-
-  x = digitalRead(13);
-  if (x == HIGH) {
-    stripe = 16;
-  }
-  if (x == LOW) {
-    stripe = 16;
-  }
-  while ((true)) {
-    prevstate = x;
-    x = digitalRead(13);
-    if (x != prevstate && x == 1) {
-      rotation++;
-    }
-    if (rotation <= stripe) {
-      analogWrite(lmf, 155);
-      analogWrite(rmb, 155);
-      digitalWrite(rmf, LOW);
-      digitalWrite(lmb, LOW);
-    }
-    else {
-
-      digitalWrite(lmf, LOW);
-      digitalWrite(rmb, LOW);
-      break;
-    }
-
-
-  }
 }
 
 
 void rightTurn() {
-  bool x;
-  bool prevstate;
-  rotation = 0;
-
-  int stripe = 0;
-  x = digitalRead(13);
-  if (x == HIGH) {
-    stripe = 8;
-  }
-  if (x == LOW) {
-    stripe = 8;
-  }
-
-
-
+  int stripe = 8;
   while (true) {
     // put your main code here, to run repeatedly:
 
-    prevstate = x;
-    x = digitalRead(13);
-    if (x != prevstate && x == 1) {
-      rotation++;
-    }
+   ir();
     if (rotation <= stripe) {
       analogWrite(lmf, 155);
       analogWrite(rmb, 155);
@@ -194,11 +98,9 @@ void rightTurn() {
 void forward() {
 
 
-  bool x;
-  bool prevstate;
-  rotation = 0;
+  
   bool stoper = false;
-  int distance = 0;
+
   while (true) {
     digitalWrite(trigPin_s, LOW);
     delayMicroseconds(2);
@@ -214,14 +116,10 @@ void forward() {
       stopBot();
       break;
     }
-    prevstate = x;
-    x = digitalRead(13);
-    if (x != prevstate && x == 1) {
-      rotation++;
-    }
-    distance = rotation * 21 / 10;
+    
+    ir();
 
-    if (distance <= 30) {
+    if (rotation == 14) {
       digitalWrite(lmf, HIGH);
       digitalWrite(rmf, HIGH);
       digitalWrite(lmb, LOW);
@@ -239,6 +137,16 @@ void forward() {
 
 
 }
+void ir(){
+  bool x;
+  bool prevstate;
+  rotation = 0;
+  prevstate = x;
+    x = digitalRead(13);
+    if (x != prevstate && x == 1) {
+      rotation++;
+    }
+  }
 
 void stopBot() {
   analogWrite(lmb, 0);
@@ -246,68 +154,6 @@ void stopBot() {
   analogWrite(lmf, 0);
   analogWrite(rmf, 0);
 }
-void hazard(){
-
-   bool x;
-  bool prevstate;
-  while (true) {
-
-    prevstate = x;
-    x = digitalRead(13);
-    if (x != prevstate && x == 0&&rotation>0) {
-      rotation--;
-    }
-    if (rotation>0){
-      digitalWrite(lmf, LOW);
-      digitalWrite(rmf, LOW);
-       digitalWrite(lmb, HIGH);
-      digitalWrite(rmb, HIGH);
-      }
-       else {
-      digitalWrite(lmf, LOW);
-      digitalWrite(rmf, LOW);
-      digitalWrite(lmb, LOW);
-      digitalWrite(rmb, LOW);
-      break;
-    }
-
-  }
-  uturn();
-
-    for (int x = 0; x < count - last[lstcounter-1]; x++) {
-      Serial.println(arr[count-x]);
-      if (arr[count - x] == 2) {
-        rightTurn();
-        arr[count - x] = 0;
-
-      }
-      if (arr[count - x] == -2) {
-        leftTurn();
-        arr[count - x] = 0;
-      }
-      if (arr[count - x] == 1) {
-        forward();
-        arr[count - x] = 0;
-      }
-
-
-    }
-    count = last[lstcounter];
-    last[lstcounter] = 0;
-    lstcounter--;
-    if(togo[tgcount-1]==-2){
-    leftTurn();
-
-    }
-     if(togo[tgcount-1]==2){
-    rightTurn();
-
-    }
-     if(togo[tgcount-1]==1){
-    forward();
-
-    }
-  }
 void BackItUp(){
    bool x;
   bool prevstate;
@@ -315,7 +161,7 @@ void BackItUp(){
 
     prevstate = x;
     x = digitalRead(13);
-    if (x != prevstate && x == 0 && rotation>0 ) {
+    if (x != prevstate && x == 1 && rotation>0 ) {
       rotation--;
     }
     if (rotation>0){
@@ -335,6 +181,29 @@ void BackItUp(){
 
   }
 }
+void cursedFront(){
+     forward();
+    visitall();
+    BackItUp();
+  }
+void cursedRight(){
+      rightTurn();
+    forward();
+    leftTurn();
+    visitall();
+    rightTurn();
+    BackItUp();
+    leftTurn();
+  }
+void cursedLeft(){
+    leftTurn();
+    forward();
+    rightTurn();
+    visitall();
+    leftTurn();
+    BackItUp();
+    rightTurn();
+  }
 void visitall() {
   Serial.println("visitall");
   delay(1000);
@@ -407,28 +276,30 @@ void visitall() {
   
  
 
-  if (front == true) {
-    forward();
-    visitall();
-    BackItUp();
+  if (front && !left && !right) {
+      cursedFront();
      }
-      if (right == true) {
-    rightTurn();
-    forward();
-    leftTurn();
-    visitall();
-    rightTurn();
-    BackItUp();
-    leftTurn();
+      if (right && !left && !front) {
+      cursedRight();
   }
-   if (left == true) {
-    leftTurn();
-    forward();
-    rightTurn();
-    visitall();
-    leftTurn();
-    BackItUp();
-    rightTurn();
+   if (left && !right && !front) {
+      cursedLeft();
   }
-
+  if (right && left && !front){
+    cursedRight();
+    cursedLeft();
+    }
+     if (right && !left && front){
+    cursedFront();
+    cursedRight();
+    }
+     if (!right && left && front){
+    cursedFront();
+    cursedLeft();
+    }
+     if (right && left && front){
+     cursedFront();
+    cursedRight();
+    cursedLeft();
+    }
 }
